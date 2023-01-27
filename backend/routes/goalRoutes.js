dc36af299af765c8da8d2d54e8915916c1049c1e
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-01-27 12:44:13 -0800

    clean up routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,9 +7,7 @@ const {
   deleteGoal 
 } = require('../controllers/goalController');
 
-router.get('/', getGoals);
-router.post('/', setGoal);
-router.put('/:id', updateGoal);
-router.delete('/:id', deleteGoal);
+router.route('/').get(getGoals).post(setGoal);
+router.route('/:id').put(updateGoal).delete(deleteGoal);
 
 module.exports = router;
