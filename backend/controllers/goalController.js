37ef45d699be9a96f8fdf9325107b474600b7b0b
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-01-27 12:54:38 -0800

    accept body data

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,6 +3,8 @@ const getGoals = (req, res) => {
 };
 
 const setGoal = (req, res) => {
+  console.log(req.body);
+  
   res.status(200).json({message: 'Set Goal'});
 };
 
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
