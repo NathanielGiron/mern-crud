dbcad3e1e0561bb11d91288835eb8e202a0604ec
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-01-27 13:27:10 -0800

    added express async handler for mongoose to handle promises

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,23 +1,25 @@
-const getGoals = (req, res) => {
-  res.status(200).json({message: 'Get Goals'});
-};
+const asyncHandler = require('express-async-handler');
 
-const setGoal = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
+  res.status(200).json({message: 'Get Goals'});
+});
+
+const setGoal = asyncHandler(async (req, res) => {
   if(!req.body.text) {
     res.status(400);
     throw new Error('Please add a text field');
   }
 
   res.status(200).json({message: 'Set Goal'});
-};
+});
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({message: `Update goal ${req.params.id}`});
-};
+});
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({message: `Delete goal ${req.params.id}`});
-}
+});
 
 module.exports = {
   getGoals, 
