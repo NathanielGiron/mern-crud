17abdd32f35d3fa674861af2e9d519889d87a818
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-01-27 23:35:23 -0800

    added logic for set goal and update goal, fixed deprecation warning

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -4,6 +4,7 @@ const Goal = require('../models/goalModel');
 
 const getGoals = asyncHandler(async (req, res) => {
   const goals = await Goal.find();
+
   res.status(200).json(goals);
 });
 
@@ -13,11 +14,24 @@ const setGoal = asyncHandler(async (req, res) => {
     throw new Error('Please add a text field');
   }
 
-  res.status(200).json({message: 'Set Goal'});
+  const goal = await Goal.create({
+    text: req.body.text
+  });
+
+  res.status(200).json(goal);
 });
 
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({message: `Update goal ${req.params.id}`});
+  const goal = await Goal.findById(req.params.id);
+
+  if(!goal) {
+    res.status(400);
+    throw new Error('Goal not found');
+  }
+  
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
+  
+  res.status(200).json(updatedGoal);
 });
 
 const deleteGoal = asyncHandler(async (req, res) => {
