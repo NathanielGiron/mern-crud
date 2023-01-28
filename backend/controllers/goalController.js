a140f1e4ee0cfdf39a0df72b42ed635542fa8c75
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-01-27 23:44:36 -0800

    added delete goal logic

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -35,7 +35,16 @@ const updateGoal = asyncHandler(async (req, res) => {
 });
 
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({message: `Delete goal ${req.params.id}`});
+  const goal = await Goal.findById(req.params.id);
+
+  if(!goal) {
+    res.status(400);
+    throw new Error('Goal not found');
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
