eed28b00d98bf4260897c550af8c83c1bc7324cb
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-01-29 11:56:29 -0800

    authenticate user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,7 +40,20 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'Login User' })
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if(user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials');
+  }
 });
 
 const currentUser = asyncHandler(async (req, res) => {
