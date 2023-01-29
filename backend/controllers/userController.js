10bb17742b66a304ed302e7acd965cdc095a73c7
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-01-29 12:13:21 -0800

    generate jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,7 +31,8 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     });
   } else {
     res.status(400);
@@ -48,7 +49,8 @@ const loginUser = asyncHandler(async (req, res) => {
     res.json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     });
   } else {
     res.status(400);
@@ -59,6 +61,10 @@ const loginUser = asyncHandler(async (req, res) => {
 const currentUser = asyncHandler(async (req, res) => {
   res.json({ message: 'User data' })
 });
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+}
 
 module.exports = {
   registerUser,
