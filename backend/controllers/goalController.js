42829ed805598bcdd667b86ba00e4c082d2b031f
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-01-29 13:39:24 -0800

    get and set a user goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -3,7 +3,7 @@ const asyncHandler = require('express-async-handler');
 const Goal = require('../models/goalModel');
 
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
 
   res.status(200).json(goals);
 });
@@ -15,6 +15,7 @@ const setGoal = asyncHandler(async (req, res) => {
   }
 
   const goal = await Goal.create({
+    user: req.user.id,
     text: req.body.text
   });
 
