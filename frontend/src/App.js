6d5b53968c50e75219d906c4f7572af156492052
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-02-02 12:39:36 -0800

    setup react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <Router>
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Dashboard />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
