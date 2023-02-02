e90ae1d3e0320b7ac6256fac9c2ed17ce66ca53b
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-02-02 12:50:04 -0800

    added header with nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -7,6 +8,7 @@ function App() {
   return (
     <Router>
       <div className='container'>
+        <Header />
         <Routes>
           <Route path='/' element={<Dashboard />}></Route>
           <Route path='/login' element={<Login />}></Route>
