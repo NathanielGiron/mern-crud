21e712e399d4580c141821f24c91c9e966f8050d
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-02-08 09:28:22 -0800

    extra reducers for register

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -42,6 +42,23 @@ export const authSlice = createSlice({
       state.message = "";
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      });
+  },
 });
 
 export const { reset } = authSlice.actions;
