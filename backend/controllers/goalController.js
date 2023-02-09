950adda2d06eb85de9e3912065c50631cfc578c3
Nathaniel Giron <nathaniel.giron@gmail.com>
2023-02-09 10:17:00 -0800

    fixed getting user.id twice

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,7 @@
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
 
-const Goal = require('../models/goalModel');
-const User = require('../models/userModel');
+const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 const getGoals = asyncHandler(async (req, res) => {
   const goals = await Goal.find({ user: req.user.id });
@@ -10,14 +10,14 @@ const getGoals = asyncHandler(async (req, res) => {
 });
 
 const setGoal = asyncHandler(async (req, res) => {
-  if(!req.body.text) {
+  if (!req.body.text) {
     res.status(400);
-    throw new Error('Please add a text field');
+    throw new Error("Please add a text field");
   }
 
   const goal = await Goal.create({
     user: req.user.id,
-    text: req.body.text
+    text: req.body.text,
   });
 
   res.status(200).json(goal);
@@ -26,46 +26,44 @@ const setGoal = asyncHandler(async (req, res) => {
 const updateGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
 
-  if(!goal) {
+  if (!goal) {
     res.status(400);
-    throw new Error('Goal not found');
+    throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
-  if(!user) {
+  if (!req.user) {
     res.status(401);
-    throw new Error('User not found');
+    throw new Error("User not found");
   }
 
-  if(goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
-    throw new Error('User not authorized');
+    throw new Error("User not authorized");
   }
-  
-  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
-  
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
   res.status(200).json(updatedGoal);
 });
 
 const deleteGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
 
-  if(!goal) {
+  if (!goal) {
     res.status(400);
-    throw new Error('Goal not found');
+    throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
-  if(!user) {
+  if (!req.user) {
     res.status(401);
-    throw new Error('User not found');
+    throw new Error("User not found");
   }
 
-  if(goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
-    throw new Error('User not authorized');
+    throw new Error("User not authorized");
   }
 
   await goal.remove();
@@ -74,8 +72,8 @@ const deleteGoal = asyncHandler(async (req, res) => {
 });
 
 module.exports = {
-  getGoals, 
-  setGoal, 
-  updateGoal, 
-  deleteGoal
-}
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,42 +1,42 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const asyncHandler = require('express-async-handler');
-const User = require('../models/userModel');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
-  if(!name || !email || !password) {
+  if (!name || !email || !password) {
     res.status(400);
-    throw new Error('Please fill out the fields');
+    throw new Error("Please fill out the fields");
   }
 
-  const userExists = await User.findOne({email});
+  const userExists = await User.findOne({ email });
 
-  if(userExists) {
+  if (userExists) {
     res.status(400);
-    throw new Error('User already exist');
+    throw new Error("User already exist");
   }
 
   const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt)
+  const hashedPassword = await bcrypt.hash(password, salt);
 
   const user = await User.create({
     name,
     email,
-    password: hashedPassword
+    password: hashedPassword,
   });
 
-  if(user) {
+  if (user) {
     res.status(201).json({
       _id: user.id,
       name: user.name,
       email: user.email,
-      token: generateToken(user._id)
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
-    throw new Error('Invalid user data');
+    throw new Error("Invalid user data");
   }
 });
 
@@ -45,35 +45,29 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const user = await User.findOne({ email });
 
-  if(user && (await bcrypt.compare(password, user.password))) {
+  if (user && (await bcrypt.compare(password, user.password))) {
     res.json({
       _id: user.id,
       name: user.name,
       email: user.email,
-      token: generateToken(user._id)
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
-    throw new Error('Invalid credentials');
+    throw new Error("Invalid credentials");
   }
 });
 
 const currentUser = asyncHandler(async (req, res) => {
-  const { _id, name, email } = await User.findById(req.user.id);
-
-  res.status(200).json({
-    id: _id,
-    name,
-    email
-  });
+  res.status(200).json(req.user);
 });
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
-}
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
 
 module.exports = {
   registerUser,
   loginUser,
-  currentUser
-}
+  currentUser,
+};
